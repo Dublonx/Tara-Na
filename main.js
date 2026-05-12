76447bf71c593738e5a1f7328c877a78395598ce
Dublonx <dublonjmx17@gmail.com>
2026-05-12 19:52:54 +0800

    Update html, css, and js file :-D

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const nav = document.querySelector('#nav');
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+// close navigation
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
