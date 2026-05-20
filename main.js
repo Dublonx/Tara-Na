ca5efc73343de69b4ed6ad5c017b89c1fc083e62
Dublonx <dublonjmx17@gmail.com>
2026-05-20 22:27:36 +0800

    Changed icon for menu bar

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const nav = document.querySelector('#nav');
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
+    
+    // changed icon
+    if (nav.classList.contains("active")) {
+        menu.className = "fa-solid fa-xmark";
+    } else {
+        menu.className = "fa-solid fa-bars";
+    }
 });
 
 // close navigation
@@ -12,6 +19,8 @@ const links = document.querySelectorAll('.link');
 links.forEach(link => {
     link.addEventListener("click", () => {
         nav.classList.remove("active");
+        
+        menu.className = "fa-solid fa-bars";
     });
 });
 
